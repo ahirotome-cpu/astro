f94a3c898874b98b97682a90a5096bd756a55a44
Galia <galia.musllimova@gmail.com>
2026-04-11 09:04:03 +0500

    add form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import styles from './page.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  
   return (
     <main className={styles.container}>
       <h1>Узнай, какие у тебя отношения</h1>
 
-      <button className={styles.button}>
+      <button className={styles.button} onClick={() => router.push('/form')}>
         Разобрать мои отношения
       </button>
     </main>
